9636c451005f431aa7609c6cc3ff9dd235834395
Huimin Jiang <hjiang7@mail.sfsu.edu>
2020-02-29 00:26:32 -0800

    Chapter 2.3 homework

--- 2020-03-02-FounderButtons-Huimin-Jiang/ViewController.swift
+++ 2020-03-02-FounderButtons-Huimin-Jiang/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabelOne: UILabel!
+    
+    @IBOutlet weak var messageLabelTwo: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressedOne(_ sender: UIButton) {
+        messageLabelOne.text = "Larry Page"
+        messageLabelTwo.text = "Sergey Brin"
+    }
+    
+    
+    @IBAction func buttonPressedTwo(_ sender: UIButton) {
+        messageLabelOne.text = "Jennifer Hyman"
+        messageLabelTwo.text = "Jenny Fleiss"
+    }
+    
+    
+    @IBAction func buttonClear(_ sender: UIButton) {
+        messageLabelOne.text = ""
+        messageLabelTwo.text = ""
+    }
 }
 
